41539554a87f1f039fecb3fe9ee6ad98873ece17
Wondmagegn Merkeb <wondmagegnmerkebbeleka@gmail.com>
2025-07-19 09:56:42 +0000

    on login and registor not use header and footer

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,17 +1,13 @@
-import PublicLayout from '../(public)/layout';
-
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <PublicLayout>
-      <div className="flex flex-grow items-center justify-center p-4 sm:p-6 lg:p-8">
-        <div className="w-full max-w-md">
-          {children}
-        </div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <div className="w-full max-w-md">
+        {children}
       </div>
-    </PublicLayout>
+    </main>
   );
 }
